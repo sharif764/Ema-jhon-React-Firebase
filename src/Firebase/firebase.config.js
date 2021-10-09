cca0acfe64d1ee5223ef6188125c0b98e31a7249
sharif764 <ss1884088@gmail.com>
2021-10-09 22:55:38 +0600

    J

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAIRMYAeLSiH8nRdOKCIygYJvp_M96EBKU",
+    authDomain: "ema-jhon-react-firebase.firebaseapp.com",
+    projectId: "ema-jhon-react-firebase",
+    storageBucket: "ema-jhon-react-firebase.appspot.com",
+    messagingSenderId: "304228739624",
+    appId: "1:304228739624:web:4c07dd5e1f34dcbf8de31d"
+};
+export default firebaseConfig;
